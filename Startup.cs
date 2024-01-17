fa019704058c31354cfbb8ab1dd3a21ce7914d12
IvanPopovski0 <156969232+IvanPopovski0@users.noreply.github.com>
2024-01-18 00:27:46 +0100

    BACKEND
    some of the backend files

---
+++ Startup.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+
+public class Startup
+{
+    public void ConfigureServices(IServiceCollection services)
+    {
+        // Configure services here
+    }
+
+    public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+    {
+        // Configure middleware here
+    }
+}
