b827c3719012b4614ec157144af40360334b3562
IvanPopovski0 <156969232+IvanPopovski0@users.noreply.github.com>
2024-01-18 00:28:15 +0100

    BACKEND
    more of the backend files

--- Startup.cs
+++ Startup.cs
@@ -1,16 +1,48 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
+using System.Configuration;
+using Microsoft.Extensions.Configuration;
 
-public class Startup
+
+namespace backend
 {
-    public void ConfigureServices(IServiceCollection services)
+    public class Startup
     {
-        // Configure services here
-    }
+        public IConfiguration Configuration { get; }
 
-    public void Configure(IApplicationBuilder app, IHostingEnvironment env)
-    {
-        // Configure middleware here
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddIdentity<IdentityUser, IdentityRole>()
+                .AddEntityFrameworkStores<ApplicationDbContext>()
+                .AddDefaultTokenProviders();
+
+            services.AddDbContext<ApplicationDbContext>(options =>
+            options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddCors(options =>
+            {
+                options.AddPolicy("AllowAll",
+                    builder =>
+                    {
+                        builder.AllowAnyOrigin()
+                               .AllowAnyMethod()
+                               .AllowAnyHeader();
+                    });
+            });
+
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseCors("AllowAll");
+
+        }
     }
 }
